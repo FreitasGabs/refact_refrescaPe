5d7fd3731bcfac4eaf5470f4127513050393432f
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-10-27 14:50:19 -0300

    :lipstick: Header pronta

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,13 @@ import "./Header.scss"
 export default function Header() {
     return (
         <header>
-            
+            <img src="/src/assets/RefrescaPeTopo.png" alt="Logotipo da empresa fictícia Refesca Pé Pisos Frios, sendo um pé e com o nome da empresa" />
+
+            <ul>
+                <li>Rua da Olaria, 3º Piso</li>
+                <li>Tel: (21) 98881-2244</li>
+                <li><a href="mailto:contato@refrescape.com.br">contato@refrescape.com.br</a></li>
+            </ul>
         </header>
     )
 }
