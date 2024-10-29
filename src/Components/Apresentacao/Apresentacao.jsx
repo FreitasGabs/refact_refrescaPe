eddf447dfe85eafaca68782a92056751251f70e4
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-10-29 11:39:03 -0300

    :lipstick: Apresentação pronta

--- src/Components/Apresentacao/Apresentacao.jsx
+++ src/Components/Apresentacao/Apresentacao.jsx
@@ -3,6 +3,9 @@ import "./Apresentacao.scss"
 export default function Apresentacao() {
     return (
         <section>
+            <img class="logo" src="/src/assets/RefrescaPeLogo.png" alt="Logotipo da empresa fictícia Refresca Pé Pisos Frios, um pé e com o nome da empresa abaixo" />
+
+            <p>A melhor seleção de Pisos Cerâmicos e Rodapés para sua casa!</p>
 
         </section>
     )
